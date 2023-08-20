b66342d30f9ec2d86dffc987f3c8ee88c0a59ba0
kimmiri <kimmiri@kimmiriui-MacBookPro.local>
2023-08-20 21:38:59 +0900

    create app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="button-test">버튼</div>
+    </div>
+  );
+}
+
+export default App;
